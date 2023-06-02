d52a86b7327067fa501c89a63a08dfe4f2d69934
Flor <florvega1598@gmail.com>
2023-06-02 10:11:37 -0300

    Update three-component.component.ts

--- src/app/three-component/three-component.component.ts
+++ src/app/three-component/three-component.component.ts
@@ -19,10 +19,26 @@ export class ThreeComponentComponent implements OnInit {
     document.getElementById('canvas-container')?.appendChild(renderer.domElement);
 
     // Crear una geometría y un material
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const moon = new THREE.Mesh(geometry, material);
-scene.add(moon);
+    const SVGLoader = new THREE.SVGLoader();
+    loader.load('ruta-de-la-imagen.svg', function (data) {
+      const paths = data.paths;
+      const group = new THREE.Group();
+    
+      paths.forEach(function (path) {
+        const shapes = path.toShapes(true);
+        shapes.forEach(function (shape) {
+          const geometry = new THREE.ExtrudeGeometry(shape, {
+            depth: 10, // Ajusta la profundidad de la extrusión según tus necesidades
+            bevelEnabled: false // Desactiva el biselado si no lo deseas
+          });
+          const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+          const mesh = new THREE.Mesh(geometry, material);
+          group.add(mesh);
+        });
+      });
+    
+      scene.add(group);
+    });
 
    
     // Configurar la posición de la cámara
